3d85a18dc8a75d52b4bc73cab8a5ec819aca42b2
Jaskanwal Pawar <jpawar@pivotal.io>
2021-02-26 22:50:02 -0800

    Add Ruby package and use dired on project open
    - i.e. when opening a proejct for the first time, open dired instead of
    doom's default pop-up file browser thingy
    
    Authored-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- config.el
+++ config.el
@@ -48,6 +48,8 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-spacegrey)
 
+(setq +workspaces-switch-project-function 'dired)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
