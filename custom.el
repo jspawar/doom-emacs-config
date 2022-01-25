2a23c3184f958378573f521bb0f3806f1f0fbc43
Jaskanwal Pawar <32091345+jspawar@users.noreply.github.com>
2022-01-25 11:48:09 -0800

    Commit `custom.el`
    - Guess this gets created whenever you make a `.dir-locals.el`

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((rspec-spec-command . "script/test")
+     (rspec-use-relative-path . t)
+     (rspec-use-bundler-when-possible))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-error ((t (:background "color-160"))))
+ '(flycheck-info ((t (:background "#49658D"))))
+ '(flycheck-warning ((t (:background "#ECBE7B")))))
